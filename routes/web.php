2287d6c09f3d79892ff15a471d3d5c8d88de5b08
bryanalfredoxd <bryanalfredoxd@gmail.com>
2026-02-06 23:50:40 -0400

    prueba 3

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return Inertia::render('welcome', [
@@ -15,3 +17,16 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
+
+
+// Conexion con el servidor
+
+Route::post('/deploy-webhook-secret-123', function () {
+    // Esto ejecuta el pull desde PHP de forma interna
+    $output = shell_exec('git pull origin main 2>&1');
+    
+    // Opcional: Limpiar caché después del pull
+    Artisan::call('optimize:clear');
+    
+    return response()->json(['output' => $output]);
+});
